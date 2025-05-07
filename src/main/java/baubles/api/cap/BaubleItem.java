9da64641bfb390a71ab721baef584e455b69d30e
darktire <3216028576@qq.com>
2025-05-07 19:25:09 +0800

    fix compat with Dark-Utilities.

--- src/main/java/baubles/api/cap/BaubleItem.java
+++ src/main/java/baubles/api/cap/BaubleItem.java
@@ -1,7 +1,32 @@
 package baubles.api.cap;
 
+import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import net.minecraft.item.ItemStack;
 
 public class BaubleItem implements IBauble {
-	public BaubleItem() {}
+
+	private BaubleType type;
+
+	public BaubleItem() {
+		this.type = BaubleType.TRINKET;
+	}
+
+	public BaubleItem(BaubleType type) {
+		this.type = type;
+	}
+
+	@Override
+	public BaubleType getBaubleType() {
+		return type;
+	}
+
+	/**
+	 * Keep this for old api.
+	 * @deprecated prefer calling {@link BaubleItem#getBaubleType()} wherever possible
+	 */
+	@Override
+	public BaubleType getBaubleType(ItemStack itemStack) {
+		return type;
+	}
 }
