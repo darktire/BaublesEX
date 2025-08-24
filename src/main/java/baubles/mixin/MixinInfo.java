067dba6cc9a586fd273bf6c5f68f053f2ec560b8
darktire <3216028576@qq.com>
2025-08-24 18:01:52 +0800

    - fix detection of mod load - improve build.gradle

--- src/main/java/baubles/mixin/MixinInfo.java
+++ src/main/java/baubles/mixin/MixinInfo.java
@@ -2,32 +2,51 @@ package baubles.mixin;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Deque;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MixinInfo {
 
-    public static boolean isModLoaded(String modId, ClassLoader loader) {
+    public static boolean isModLoaded(String modId, Deque<ClassLoader> queue) {
         Boolean flag = isModLoaded.get(modId);
         if (flag == null) {
             String className = modMainClass.get(modId);
             if (className != null) {
-                try {
-                    Class.forName(className, false, loader);
-                    isModLoaded.put(modId, true);
-                    return true;
-                }
-                catch (ClassNotFoundException ignored) {
-                    isModLoaded.put(modId, false);
-                }
+                flag = exists(className, queue);
+                isModLoaded.put(modId, flag);
+                return flag;
             }
             return false;
         }
-        else return flag;
+        return flag;
     }
 
     public static String getTargetModId(String mixinClassName) {
         return mixinTargetMod.get(mixinClassName);
+    }
+
+    private static boolean exists(String className, Deque<ClassLoader> queue) {
+        String resourceName = className.replace('.', '/') + ".class";
+        return queue.stream().anyMatch(loader -> {
+            if (loader.getResource(resourceName) != null) {
+                return true;
+            }
+
+            if (loader instanceof URLClassLoader) {
+                try {
+                    Enumeration<URL> urls = ((URLClassLoader) loader).findResources(resourceName);
+                    if (urls != null && urls.hasMoreElements()) {
+                        return true;
+                    }
+                } catch (IOException ignored) {}
+            }
+            return false;
+        });
     }
 
     private static final Map<String, Boolean> isModLoaded = new HashMap<>();
@@ -50,7 +69,7 @@ public class MixinInfo {
             .build();
 
     private static final Map<String, String> modMainClass = ImmutableMap.<String, String>builder()
-            .put("mobends", "goblinbob.mobends.standard.main.MoBends")
+            .put("mobends", "goblinbob.mobends.standard.main.ModStatics")
             .put("wings", "me.paulf.wings.WingsMod")
             .build();
 }
--- src/main/java/baubles/mixin/MixinPlugin.java
+++ src/main/java/baubles/mixin/MixinPlugin.java
@@ -6,16 +6,21 @@ import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Set;
 
 public class MixinPlugin implements IMixinConfigPlugin {
-    private final ClassLoader loader = this.getClass().getClassLoader();
+    private final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    private final Deque<ClassLoader> queue = new ArrayDeque<>();
     private static final Logger log = LogManager.getLogger("BAUBLES_MIXIN");
 
     @Override
     public void onLoad(String mixinPackage) {
-
+        for (ClassLoader cur = this.loader; cur != null; cur = cur.getParent()) {
+            queue.addLast(cur);
+        }
     }
 
     @Override
@@ -26,11 +31,8 @@ public class MixinPlugin implements IMixinConfigPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         String modId = MixinInfo.getTargetModId(mixinClassName);
-        boolean flag = false;
-        if (modId != null) {
-            flag = MixinInfo.isModLoaded(modId, this.loader);
-        }
-        if (!flag) warn("Mixin for " + targetClassName + " loading failed");
+        boolean flag = MixinInfo.isModLoaded(modId, this.queue);
+        if (!flag) this.warn(targetClassName, modId);
         return flag;
     }
 
@@ -54,7 +56,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
 
     }
 
-    private static void warn(String s) {
-        log.warn(s);
+    private void warn(String s1, String s2) {
+        log.warn(String.format("Mixin for %s loading failed. If %s wasn't installed, please ignore this warning.", s1, s2));
     }
 }
