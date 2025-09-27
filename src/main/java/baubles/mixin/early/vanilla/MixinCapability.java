ba9e9a3cbf394619621747727fd6ea78ba9ec308
darktire <3216028576@qq.com>
2025-09-27 16:15:17 +0800

    - fix #22, #23

--- src/main/java/baubles/mixin/early/vanilla/MixinCapability.java
+++ src/main/java/baubles/mixin/early/vanilla/MixinCapability.java
@@ -7,29 +7,47 @@ import baubles.api.cap.BaublesCapabilityProvider;
 import baubles.api.registries.ItemsData;
 import baubles.util.ICapabilityModifiable;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityDispatcher;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = CapabilityDispatcher.class, remap = false)
-@Implements(@Interface(iface = ICapabilityModifiable.class, prefix = "baubles$"))
+@Implements(@Interface(iface = ICapabilityModifiable.class, prefix = "bs$"))
 public abstract class MixinCapability {
 
-    @Shadow private ICapabilityProvider[] caps;
+    @Shadow
+    private ICapabilityProvider[] caps;
 
     @Unique
-    public void baubles$patchCap(ItemStack stack) {
-        for (int i = 0, j = this.caps.length; i < j; i++) {
-            ICapabilityProvider provider = this.caps[i];
-            IBauble bauble = provider.getCapability(BaublesCapabilities.CAPABILITY_ITEM_BAUBLE, null);
-            if (bauble != null && !(bauble instanceof BaublesWrapper)) {
-                if (!ItemsData.isBauble(stack.getItem())) {
-                    ItemsData.registerBauble(stack.getItem(), bauble.getBaubleType(stack).getExpansion());
+    private ItemStack bs$stack;
+    @Unique
+    private boolean bs$tryRedirect = false;
+    @Unique
+    public void bs$patchCap(ItemStack stack) {
+        this.bs$stack = stack;
+        this.bs$tryRedirect = true;
+    }
+
+    @Inject(method = "getCapability", at = @At("HEAD"))
+    private <T> void injected(Capability<T> capability, EnumFacing facing, CallbackInfoReturnable<T> cir) {
+        if (this.bs$tryRedirect && capability == BaublesCapabilities.CAPABILITY_ITEM_BAUBLE) {
+            for (int i = 0, j = this.caps.length; i < j; i++) {
+                ICapabilityProvider provider = this.caps[i];
+                IBauble bauble = provider.getCapability(BaublesCapabilities.CAPABILITY_ITEM_BAUBLE, null);
+                if (bauble != null && !(bauble instanceof BaublesWrapper)) {
+                    if (!ItemsData.isBauble(this.bs$stack.getItem())) {
+                        ItemsData.registerBauble(this.bs$stack.getItem(), bauble.getBaubleType(this.bs$stack).getExpansion());
+                    }
+                    this.caps[i] = new BaublesCapabilityProvider(this.bs$stack, provider);
+                    break;
                 }
-                provider = new BaublesCapabilityProvider(stack, provider);
-                this.caps[i] = provider;
-                break;
             }
+            this.bs$tryRedirect = false;
         }
     }
 }
