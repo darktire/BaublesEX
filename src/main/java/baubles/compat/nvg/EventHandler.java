b01a05d09199cc3b50b394b57568495be2ed4bd3
darktire <3216028576@qq.com>
2025-12-14 17:27:16 +0800

    - compat with nvg

---
+++ src/main/java/baubles/compat/nvg/EventHandler.java
@@ -0,0 +1,19 @@
+package baubles.compat.nvg;
+
+import baubles.api.BaubleTypeEx;
+import baubles.api.registries.ItemsData;
+import baubles.compat.ModOnly;
+import com.noobanidus.nvg.compat.baubles.BaubleGoggles;
+import com.noobanidus.nvg.init.Items;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@ModOnly(value = "nvg")
+public class EventHandler {
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public static void itemBaubleWrap(RegistryEvent.Register<BaubleTypeEx> event) {
+        ItemsData.registerBauble(Items.goggles, new BaubleGoggles());
+    }
+}
---
+++ src/main/java/baubles/mixin/late/nvg/MixinBaubleInventoryUtil.java
@@ -0,0 +1,19 @@
+package baubles.mixin.late.nvg;
+
+import baubles.api.cap.IBaublesItemHandler;
+import baubles.util.HookHelper;
+import com.noobanidus.nvg.compat.baubles.goggles.BaubleInventoryUtil;
+import com.noobanidus.nvg.init.Items;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = BaubleInventoryUtil.class, remap = false)
+public class MixinBaubleInventoryUtil {
+
+    @Redirect(method = "getGoggles", at = @At(value = "INVOKE", target = "Lbaubles/api/cap/IBaublesItemHandler;getStackInSlot(I)Lnet/minecraft/item/ItemStack;"))
+    private static ItemStack redirect(IBaublesItemHandler instance, int i) {
+        return HookHelper.getStack(instance, i, Items.goggles);
+    }
+}
